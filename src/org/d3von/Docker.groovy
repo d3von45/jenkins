f377b17f877ab7946416c27e2ca76d03c6d0e239
d3von <khanhduy.nguyen4520@gmail.com>
2024-07-21 16:15:44 +0700

    initial

---
+++ src/org/d3von/Docker.groovy
@@ -0,0 +1,34 @@
+package org.d3von;
+
+class Docker extends PipelineBase {
+
+
+    Docker(steps) {
+        super(steps)
+    }
+
+    def images(){
+        return sh("docker images")
+    }
+
+    def cleanUp(){
+        return sh("docker rmi -f $(docker images -aq) && docker system prune -f")
+    }
+
+    def build(String path, String imageName, String tag){
+        return sh("docker build -t ${imageName}:${tag} -f ${path} .")
+    }
+
+    def push(String imageName, String tag){
+        return sh("docker push ${imageName}:${tag}")
+    }
+
+    def pull(String imageName, String tag){
+        return sh("docker pull ${imageName}:${tag}")
+    }
+
+    // def login(String username, String password, String registry){
+        
+    // }
+
+}
