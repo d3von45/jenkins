18d6f4be111556c17aa9a3970954752593e8f59e
d3von <khanhduy.nguyen4520@gmail.com>
2024-07-27 17:43:51 +0700

    update Docker class

--- src/org/d3von/Docker.groovy
+++ src/org/d3von/Docker.groovy
@@ -2,24 +2,30 @@ package org.d3von;
 
 class Docker implements Serializable {
 
-    def images(){
-        return sh("docker images")
+    def steps
+
+    Docker(steps) {
+        this.steps = steps
     }
 
+    def images(){
+        return steps.sh("docker images")
+    }
+    
     def cleanUp(){
-        return sh("docker rmi -f \$(docker images -aq) && docker system prune -f")
+        return steps.sh("docker rmi -f \$(docker images -aq) && docker system prune -f")
     }
 
     def build(String path, String imageName, String tag){
-        return sh("docker build -t ${imageName}:${tag} -f ${path} .")
+        return steps.sh("docker build -t ${imageName}:${tag} -f ${path} .")
     }
 
     def push(String imageName, String tag){
-        return sh("docker push ${imageName}:${tag}")
+        return steps.sh("docker push ${imageName}:${tag}")
     }
 
     def pull(String imageName, String tag){
-        return sh("docker pull ${imageName}:${tag}")
+        return steps.sh("docker pull ${imageName}:${tag}")
     }
 
     // def login(String username, String password, String registry){
