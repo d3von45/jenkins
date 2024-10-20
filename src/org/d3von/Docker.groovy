0604c0a8a655ea6cdfb0d3ac48aab8000401142e
d3von <khanhduy.nguyen4520@gmail.com>
2024-10-20 22:23:40 +0700

    debug not print outputs

--- src/org/d3von/Docker.groovy
+++ src/org/d3von/Docker.groovy
@@ -9,27 +9,32 @@ class Docker implements Serializable {
     }
 
     def images(){
-        steps.sh(label: 'Docker list all images output', script: 'docker images', returnStdout: true)
+        final String outputs = steps.sh(label: 'Docker list all images output', script: 'docker images', returnStdout: true)
+        steps.echo(outputs)
         return this
     }
     
     def cleanUp(){
-        steps.sh(label: 'Docker clean up output', script: 'docker images', returnStdout: true)
+        final String outputs = steps.sh(label: 'Docker clean up output', script: 'docker images', returnStdout: true)
+        steps.echo(outputs)
         return this
     }
 
     def build(){
-        steps.sh(label: 'Docker build image output', script: 'docker images', returnStdout: true)
+        final String outputs = steps.sh(label: 'Docker build image output', script: 'docker images', returnStdout: true)
+        steps.echo(outputs)
         return this
     }
 
     def push(){
-        steps.sh(label: 'Docker push image output', script: 'docker images', returnStdout: true)
+        final String outputs = steps.sh(label: 'Docker push image output', script: 'docker images', returnStdout: true)
+        steps.echo(outputs)
         return this
     }
 
     def pull(){
-        steps.sh(label: 'Docker pull image output', script: 'docker pull nginx', returnStdout: true)
+        final String outputs = steps.sh(label: 'Docker pull image output', script: 'docker pull nginx', returnStdout: true)
+        steps.echo(outputs)
         return this
     }
 
